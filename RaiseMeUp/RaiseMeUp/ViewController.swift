ae34f0e9985e1d7a514d7a9f0537006bde64e498
홍석현 <ghdtjrgus333@naver.com>
2023-10-31 09:43:31 +0900

    init. 첫 프로젝트 생성

---
+++ RaiseMeUp/RaiseMeUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RaiseMeUp
+//
+//  Created by 홍석현 on 2023/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
