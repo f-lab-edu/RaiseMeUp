5d85d7f38b66085c5f5f66101c9867d874c7edf4
홍석현 <ghdtjrgus333@naver.com>
2023-11-27 17:15:02 +0900

    feat. 해당 데이터 모델 생성

--- RaiseMeUp/RaiseMeUp/Sources/Data/DataMapping/PullUpProgramDTO.swift
+++ RaiseMeUp/RaiseMeUp/Sources/Data/DataMapping/PullUpProgramDTO.swift
@@ -1,8 +1,19 @@
 //
-//  PullupProgramDTO.swift
+//  PullUpProgramDTO.swift
 //  RaiseMeUp
 //
 //  Created by 홍석현 on 11/27/23.
 //
 
 import Foundation
+
+struct PullUpProgramDTO: Decodable {
+    var programs: [ProgramDTO]
+
+}
+
+struct ProgramDTO: Decodable {
+    var name: String
+    var description: String
+    var schedule: [[Int?]]
+}
