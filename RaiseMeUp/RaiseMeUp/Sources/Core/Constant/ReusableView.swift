d3a5e378b0f25946610fd966cceaa50123f20c06
홍석현 <ghdtjrgus333@naver.com>
2023-11-27 17:15:28 +0900

    chor. reusable view, tableview extension 추가

--- RaiseMeUp/RaiseMeUp/Sources/Core/Constant/ReusableView.swift
+++ RaiseMeUp/RaiseMeUp/Sources/Core/Constant/ReusableView.swift
@@ -5,4 +5,16 @@
 //  Created by 홍석현 on 11/27/23.
 //
 
-import Foundation
+import UIKit
+
+public protocol ReusableView: AnyObject {
+    /// Get identifier from class
+    static var defaultReuseIdentifier: String { get }
+}
+
+public extension ReusableView where Self: UIView {
+    static var defaultReuseIdentifier: String {
+        // Set the Identifier from class name
+        return NSStringFromClass(self)
+    }
+}
--- RaiseMeUp/RaiseMeUp/Sources/Core/Extension/UITableView+Extension.swift
+++ RaiseMeUp/RaiseMeUp/Sources/Core/Extension/UITableView+Extension.swift
@@ -5,4 +5,22 @@
 //  Created by 홍석현 on 11/27/23.
 //
 
-import Foundation
+import UIKit
+
+extension UITableViewCell: ReusableView { }
+
+extension UITableView {
+    /// Register cell with automatically setting Identifier
+    public func register<T: UITableViewCell>(_: T.Type) {
+        register(T.self, forCellReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    /// Get cell with the default reuse cell identifier
+    public func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell: \(T.self) with identifier: \(T.defaultReuseIdentifier)")
+        }
+        
+        return cell
+    }
+}
