7b7b32b6f5e99f61328f6cd046ce9dcf94334248
홍석현 <ghdtjrgus333@naver.com>
2024-02-12 17:37:14 +0900

    feat. 리스트 row 뷰

--- RaiseMeUp/RaiseMeUp/Sources/Features/Main/ExericseList/ExerciseRow.swift
+++ RaiseMeUp/RaiseMeUp/Sources/Features/Main/ExericseList/ExerciseRow.swift
@@ -5,4 +5,27 @@
 //  Created by 홍석현 on 2/6/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ExerciseRow: View {
+    var routine: ProgramTableViewCellModel
+    
+    var body: some View {
+        HStack {
+            Text(routine.day)
+            Spacer()
+            Text(routine.routine)
+        }
+    }
+}
+
+#Preview {
+    ExerciseRow(
+        routine: ProgramTableViewCellModel(
+            DailyRoutine(
+                day: "Day 02",
+                routine: [3, 2, 1, 1]
+            )
+        )
+    )
+}
