50a118c63a17dd0602159206bd4885137e62b662
홍석현 <ghdtjrgus333@naver.com>
2024-02-12 17:38:14 +0900

    feat. 메인뷰의 뷰모델 새로 생성

--- RaiseMeUp/RaiseMeUp/Sources/Features/Main/ExericseList/ExerciseViewModel.swift
+++ RaiseMeUp/RaiseMeUp/Sources/Features/Main/ExericseList/ExerciseViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import OSLog
+
+class ExerciseViewModel: ObservableObject {
+    private let useCase: TrainingUseCase
+    
+    @Published var program: [TrainingLevel] = []
+    
+    init(useCase: TrainingUseCase) {
+        self.useCase = useCase
+        
+        fetchProgram()
+    }
+    
+    func fetchProgram() {
+        Task {
+            let result = await useCase.getProgramList()
+            switch result {
+            case .success(let data):
+                self.program = data.program
+            case .failure(let error):
+                OSLog.message(.error, error.localizedDescription)
+            }
+        }
+    }
+}
