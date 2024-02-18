913f3d8d17db7cae34e37b2f32fbd97ccf0d5db6
홍석현 <ghdtjrgus333@naver.com>
2024-02-18 20:40:16 +0900

    chor. Button Style .plane으로 변경 -> 색상 때문

--- RaiseMeUp/RaiseMeUp/Sources/Features/Main/ExericseList/ExerciseList.swift
+++ RaiseMeUp/RaiseMeUp/Sources/Features/Main/ExericseList/ExerciseList.swift
@@ -28,6 +28,7 @@ struct ExerciseList: View {
                                 routine: rowModel
                             )
                         })
+                        .buttonStyle(.plain)
                     }
                 }
             }
