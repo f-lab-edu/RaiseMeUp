59dfe111757d004884dd773491a33702ba10292b
홍석현 <ghdtjrgus333@naver.com>
2024-03-20 09:43:19 +0900

    feat. 피쳐별 의존성 설정

--- RaiseMeUp/Features/Package.swift
+++ RaiseMeUp/Features/Package.swift
@@ -5,19 +5,45 @@ import PackageDescription
 
 let package = Package(
     name: "Features",
+    platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Features",
-            targets: ["Features"]),
+            name: "Coordinator",
+            targets: ["Coordinator"]),
+        .library(
+            name: "ExerciseCounter",
+            targets: ["ExerciseCounter"]),
+        .library(
+            name: "Main",
+            targets: ["Main"]),
+        .library(
+            name: "Root",
+            targets: ["Root"]),
+    ],
+    dependencies: [
+        .package(path: "../Domain")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Features"),
-        .testTarget(
-            name: "FeaturesTests",
-            dependencies: ["Features"]),
+            name: "Coordinator",
+            dependencies: [
+                "Main",
+                "Root",
+                "ExerciseCounter",
+                .product(name: "Data", package: "Data")
+            ]
+        )
+        .target(
+            name: "Main",
+            dependencies: [
+                "Main",
+                "Root",
+                "ExerciseCounter",
+                .product(name: "Data", package: "Data")
+            ]
+        )
+        
     ]
 )
